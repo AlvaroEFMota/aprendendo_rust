ba9d6e489c5935bc2ce6c4dd61737c591271ea90
Alvaro <alvaro11318@gmail.com>
2021-09-15 15:29:46 -0300

    multithread matrix multiplication

--- src/matrix_multiplication.rs
+++ src/matrix_multiplication.rs
@@ -1,7 +1,7 @@
 use std::sync::mpsc;
 use std::sync::Arc;
-use std::sync::Mutex;
 use std::thread;
+//use std::time::Duration;
 
 struct ThreadReturn {
     row: usize,
@@ -11,20 +11,21 @@ struct ThreadReturn {
 
 #[allow(dead_code)]
 fn row_col_mult(
-    mtx1: Arc<Mutex<Vec<Vec<f64>>>>,
-    mtx2: Arc<Mutex<Vec<Vec<f64>>>>,
+    mtx1: Arc<Vec<Vec<f64>>>,
+    mtx2: Arc<Vec<Vec<f64>>>,
     row: usize,
     column: usize,
 ) -> f64 {
     let mut result: f64 = 0f64;
-    let mtx1 = mtx1.lock().unwrap();
-    let mtx2 = mtx2.lock().unwrap();
     for i in 0..mtx1.len() {
         result += mtx1[row][i] * mtx2[i][column];
-        println!("[{}{}]", row, column);
+        /*println!("[{}{}]", row, column);
+        thread::sleep(Duration::from_secs(1)); // Uncomment to see all threads working
+        */
     }
     result
 }
+
 #[allow(unused_must_use)]
 #[allow(dead_code)]
 fn thread_mult(mtx1: &Vec<Vec<f64>>, mtx2: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
@@ -41,16 +42,9 @@ fn thread_mult(mtx1: &Vec<Vec<f64>>, mtx2: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     let mut handles = vec![];
     let (sender, receiver) = mpsc::channel::<ThreadReturn>();
 
-    let t_mtx1 = Arc::new(Mutex::new(mtx1));
-    let t_mtx2 = Arc::new(Mutex::new(mtx2));
+    let t_mtx1 = Arc::new(mtx1);
+    let t_mtx2 = Arc::new(mtx2);
 
-    /*for _ in 0..4 {
-        let t_mtx1 = Arc::clone(&t_mtx1);
-        let t_mtx2 = Arc::clone(&t_mtx2);
-        thread::spawn(move || {
-            println!("MTX {:?}", t_mtx1);
-        });
-    }*/
     for row in 0..r {
         for column in 0..c {
             let sender = sender.clone();
@@ -64,46 +58,16 @@ fn thread_mult(mtx1: &Vec<Vec<f64>>, mtx2: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     }
 
     for handle in handles {
-        handle.join().unwrap();
+        handle.join().unwrap(); // Wait all threads finish
     }
 
     for _ in 0..r*c {
         let received = receiver.recv().unwrap();
-        println!("Received: {} from [{},{}]", received.value, received.row, received.column);
+        //println!("Received: {} from [{},{}]", received.value, received.row, received.column);
         result[received.row][received.column] = received.value;
     }
-    /*for received in receiver {
-        println!("Received: {} from [{},{}]", received.value, received.row, received.column);
-        result[received.row][received.column] = received.value;
-    }*/
     result
 }
-
-
-pub fn run() {
-    let m1 = vec![
-        vec![1.0, 2.0, 5.0],
-        vec![3.0, 4.0, 6.0],
-        vec![7.0, 8.0, 9.0],
-    ];
-    let m2 = vec![
-        vec![1.0, 0.0, 0.0],
-        vec![0.0, 1.0, 0.0],
-        vec![0.0, 0.0, 1.0],
-    ];
-    let r = thread_mult(&m1, &m2);
-    assert_eq!(
-        vec![
-            vec![1.0, 2.0, 5.0],
-            vec![3.0, 4.0, 6.0],
-            vec![7.0, 8.0, 9.0]
-        ],
-        r
-    );
-}
-
-
-
 
 #[allow(dead_code)]
 fn simple_mult(mtx1: &Vec<Vec<f64>>, mtx2: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
@@ -122,12 +86,47 @@ fn simple_mult(mtx1: &Vec<Vec<f64>>, mtx2: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     result
 }
 
+pub fn run() {
+    let m1 = vec![
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+    ];
+    let m2 = vec![
+        vec![1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    ];
+    let r = thread_mult(&m1, &m2);
+    assert_eq!(m1, r);
+}
+
+
+
+
+
 #[test]
 fn simple_mult_test() {
     let m1 = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
     let m2 = vec![vec![1.0], vec![2.0]];
+
     let r = simple_mult(&m1, &m2);
     assert_eq!(vec![vec![5.0], vec![11.0]], r);
+
     let t = thread_mult(&m1, &m2);
     assert_eq!(vec![vec![5.0], vec![11.0]], t);
 
@@ -141,6 +140,7 @@ fn simple_mult_test() {
         vec![0.0, 1.0, 0.0],
         vec![0.0, 0.0, 1.0],
     ];
+
     let r = simple_mult(&m1, &m2);
     assert_eq!(
         vec![
@@ -160,4 +160,35 @@ fn simple_mult_test() {
         ],
         t
     );
+
+    let m1 = vec![
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+    ];
+    let m2 = vec![
+        vec![1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+        vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    ];
+    
+    let r = simple_mult(&m1, &m2);
+    assert_eq!(m1, r);
+
+    let t = thread_mult(&m1, &m2);
+    assert_eq!(m1, t);
 }
